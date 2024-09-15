c7be3b51500406b93b4606b6d73f81f7fce8e232
Sean Ditny <138174805+seangoodvibes@users.noreply.github.com>
2024-09-15 19:14:05 -0400

    Enable Seamless Drone Linear Recording
    When linear recording to a clip, you can seamlessly record a drone note using the audition pads or from external midi by continuing to audition / send a note until the linear recording stops. After linear recording stops, you can stop auditioning / send a note off and the drone note will persist without any breaks or re-triggering.

--- src/deluge/model/note/note_row.cpp
+++ src/deluge/model/note/note_row.cpp
@@ -2106,7 +2106,6 @@ noFurtherNotes:
 
 		// If user is auditioning note...
 		if (isAuditioning(modelStack)) {
-
 			// If they've also just recorded a note and it was quantized later, we do need to keep an eye out for it,
 			// despite the fact that we're auditioning.
 			if (effectiveForwardPos < ignoreNoteOnsBefore_) {
@@ -2122,9 +2121,8 @@ noFurtherNotes:
 
 			if (!notes.getNumElements()) {
 stopNote:
-				stopCurrentlyPlayingNote(
-				    modelStack, true,
-				    thisNote); // Ideally (but optionally) supply the note, so lift-velocity can be used
+				// Ideally (but optionally) supply the note, so lift-velocity can be used
+				stopCurrentlyPlayingNote(modelStack, true, thisNote);
 			}
 			else {
 
@@ -2178,7 +2176,7 @@ stopNote:
 				if (ticksTilNextNoteEvent <= 0) {
 
 					// If it's a droning, full-length note...
-					if (thisNote->pos == 0 && thisNote->length == effectiveLength) {
+					if (thisNote->isDrone(effectiveLength)) {
 
 						// If it's a cut-mode sample, though, we want it to stop, so it can get retriggered again from
 						// the start. Same for time-stretching - although those can loop themselves, caching comes along
@@ -4319,6 +4317,19 @@ void NoteRow::setSequenceDirectionMode(ModelStackWithNoteRow* modelStack, Sequen
 			}
 		}
 	}
+}
+
+/// check to see if this note row has only one note and that note is a drone note
+/// drone note = note at position 0 with length equal to the note row's length
+bool NoteRow::isDroning(int32_t effectiveLength) {
+	int32_t numNotes = notes.getNumElements();
+	if (numNotes == 1) {
+		Note* note = notes.getElement(0);
+		if (note->isDrone(effectiveLength)) {
+			return true;
+		}
+	}
+	return false;
 }
 
 /*
