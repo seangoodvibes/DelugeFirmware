3f5d823a7028e6240b8130a2c7b23035a9293d07
Sean Ditny <138174805+seangoodvibes@users.noreply.github.com>
2024-09-19 17:22:11 -0400

    Enhancement/ UI accessibility menu + menu alternate rendering
    Fix/ submenu scrolling + Qwerty menu inverted area (#2648)
    
    * Bugfix submenu scrolling
    
    Adjusted pixel at which scrolling stops in the submenus
    
    * Bugfix qwerty menu highlight
    
    Adjust inverted area start by 1 pixel to the left
    
    * Fix context menu invert area highlighting
    
    * Extend qwerty inverted area highlight
    
    Per feedback, extended inverted area highlight in browser menus
    
    Enhancement/ Add defaults UI accessibility menu + add alternate rendering for selected menu item (#2537)
    
    * Add alternate menu cursor + accessibility menu
    
    Add alternate rendering for selected menu item by highlighting just the left edge
    
    Move accessibility menu from community features to new Defaults > UI > Accessibility menu
    
    * typo
    
    Fix highlighting in browser (#2781)
    
    Fixed where inverted highlighting starts in preset browser when you're scrolled over into a string using <>
    
    Fix alt playback start behaviour (#2898)
    
    Fixed alternative playback start behaviour by cleaning up logic and making it more explicit in PlaybackHandler::setupPlaybackUsingInternalClock
    
    Re-added change log entry for alternative playback start behaviour that was accidentally removed

--- src/deluge/gui/ui/qwerty_ui.cpp
+++ src/deluge/gui/ui/qwerty_ui.cpp
@@ -125,13 +125,17 @@ void QwertyUI::drawTextForOLEDEditing(int32_t xPixel, int32_t xPixelMax, int32_t
 	canvas.drawString(&displayName[scrollPosHorizontal], xPixel, yPixel, kTextSpacingX, kTextSpacingY, 0,
 	                  xPixel + maxNumChars * kTextSpacingX);
 
+	int32_t highlightStartX;
 	int32_t scrollAmount = enteredTextEditPos - scrollPosHorizontal;
-
-	// if we're at the beginning of the string, we want to show an extra inverted pixel before
-	// if we're somewhere inside the string, we want to start highlight right on the character
-	int32_t startAdjustment = scrollAmount ? 0 : 1;
-
-	int32_t highlightStartX = xPixel + kTextSpacingX * scrollAmount - startAdjustment;
+	if (FlashStorage::accessibilityMenuHighlighting && !scrollAmount) {
+		highlightStartX = 0;
+	}
+	else {
+		// if we're at the beginning of the string, we want to show an extra inverted pixel before
+		// if we're somewhere inside the string, we want to start highlight right on the character
+		int32_t startAdjustment = scrollAmount ? 0 : 1;
+		highlightStartX = xPixel + kTextSpacingX * scrollAmount - startAdjustment;
+	}
 	int32_t highlightWidth = xPixelMax - highlightStartX;
 
 	if (atVeryEnd) {
@@ -142,7 +146,7 @@ void QwertyUI::drawTextForOLEDEditing(int32_t xPixel, int32_t xPixelMax, int32_t
 		}
 	}
 	else {
-		canvas.invertArea(highlightStartX, highlightWidth, yPixel, yPixel + kTextSpacingY - 1);
+		canvas.invertLeftEdgeForMenuHighlighting(highlightStartX, highlightWidth, yPixel, yPixel + kTextSpacingY - 1);
 	}
 }
 
