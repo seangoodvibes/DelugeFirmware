fed2465be7eefd049daeef88f3dc11ff8fb3f65f
Mark Adams <mark.adams@queensu.ca>
2024-10-01 10:41:34 -0400

    Feature - looper and sampler types for audio outputs (#2702)
    * create looper and sampler types for audio files
    
    * select the source when you press a clip pad in the session
    
    * fix clip rendering when changing monitoring
    
    * fix is empty check
    
    * add checks on mode
    
    * add docs
    
    * Update CHANGELOG.md
    
    Just moving audio clip change log entry to its own section, not under automation view
    
    * clarify docs
    
    ---------
    
    show a popup for audio clip type change on 7seg (#3234)
    
    * show a popup for 7seg
    
    * move popup from view to output to accomodate song view
    
    add display include to audio output
    
    Co-Authored-By: Mark Adams <mark.adams@queensu.ca>
    Co-Authored-By: Sean Ditny <138174805+seangoodvibes@users.noreply.github.com>

--- src/deluge/gui/views/audio_clip_view.cpp
+++ src/deluge/gui/views/audio_clip_view.cpp
@@ -661,8 +661,8 @@ void AudioClipView::selectEncoderAction(int8_t offset) {
 	if (currentUIMode) {
 		return;
 	}
-
-	view.navigateThroughAudioOutputsForAudioClip(offset, getCurrentAudioClip());
+	auto ao = (AudioOutput*)getCurrentAudioClip()->output;
+	ao->scrollAudioOutputMode(offset);
 }
 
 void AudioClipView::setClipLengthEqualToSampleLength() {
