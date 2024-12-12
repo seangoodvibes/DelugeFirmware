0d5930b65ef84c51cfb099a45480522b4aa19a83
Sean Ditny <138174805+seangoodvibes@users.noreply.github.com>
2024-12-11 23:10:24 -0500

    Update automation_layout.cpp

--- src/deluge/gui/views/automation/automation_layout.cpp
+++ src/deluge/gui/views/automation/automation_layout.cpp
@@ -90,15 +90,9 @@
 #include "storage/flash_storage.h"
 #include "storage/multi_range/multi_range.h"
 #include "storage/storage_manager.h"
-#include "util/cfunctions.h"
 #include "util/comparison.h"
 #include "util/functions.h"
-#include <new>
 #include <string.h>
-
-extern "C" {
-#include "RZA1/uart/sio_char.h"
-}
 
 // namespace deluge::gui::views::automation {
 
