c1fee4f55ff7fb6f0f9ead0f1e4e828c9e7fafcc
Moritz Weigt <155909144+mweigt@users.noreply.github.com>
2025-01-08 18:42:19 +0100

    Fix: When using the backspace in the keyboard when searching samples, it doesn't re-search (#3227)
    * re-search on backspace
    
    * show all files when no characters, prevent typing unmatched characters
    
    * Update src/deluge/gui/ui/browser/browser.cpp
    
    Co-authored-by: Katherine Whitlock <kate@skylinesynths.nyc>
    
    ---------
    
    Co-authored-by: Katherine Whitlock <kate@skylinesynths.nyc>

--- src/deluge/gui/ui/qwerty_ui.cpp
+++ src/deluge/gui/ui/qwerty_ui.cpp
@@ -403,6 +403,8 @@ void QwertyUI::processBackspace() {
 	}
 	if (!enteredText.isEmpty()) {
 		enteredText.shorten(enteredTextEditPos);
+		predictExtendedText();
+		predictionInterrupted = false;
 		displayText();
 	}
 }
