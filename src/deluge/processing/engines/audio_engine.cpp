6d0629c78c7418a84268fbcc09c249a8cdfc95ce
Sean Ditny <138174805+seangoodvibes@users.noreply.github.com>
2026-01-04 20:17:19 -0500

    Delegate running audio engine routine to task manager
    Instead of calling AudioEngine::routine() directly, run the corresponding task manager task so that the task manager knows the last time it was run and can continue to schedule things correctly.

--- src/deluge/processing/engines/audio_engine.cpp
+++ src/deluge/processing/engines/audio_engine.cpp
@@ -383,14 +383,32 @@ void routineWithClusterLoading(bool mayProcessUserActionsBetween, bool useYield)
 	if (!routineBeenCalled) {
 		bypassCulling = true; // yolo?
 
-		if (useYield) {
-			logAction("RWCL: yieldToAudio()");
-			yieldToAudio();
+		/*	if (useYield) {
+		        logAction("RWCL: yieldToAudio()");
+		        yieldToAudio();
+		    }
+		    else if (!audioRoutineLocked) {
+		        logAction("RWCL: routine_task()");
+		        // Sean: replace AudioEngine::routine() call with call to run AudioEngine::routine() task
+		        if (AudioEngine::routine_task_id != -1) {
+		            runTask(AudioEngine::routine_task_id);
+		        }
+		        else {
+		            AudioEngine::routine();
+		        }
+		    }
+		*/
+		//	if (!audioRoutineLocked) {
+		if (AudioEngine::routine_task_id != -1) {
+			runTask(AudioEngine::routine_task_id);
 		}
 		else {
-			logAction("RWCL: routine()");
-			routine();
+			AudioEngine::routine();
 		}
+		//	}
+		//	else {
+		//		yield([]() { return (AudioEngine::routineBeenCalled); });
+		//	}
 	}
 }
 
